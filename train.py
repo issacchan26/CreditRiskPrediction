d17a2b88ced7b3b044fcae3c2a6c799fd9c33e7f
issacchan26 <72512071+issacchan26@users.noreply.github.com>
2023-09-17 04:15:03 -0400

    Update train.py

--- train.py
+++ train.py
@@ -10,15 +10,15 @@ pd.set_option('display.max_columns', None)
 df = pd.read_csv(input_file, on_bad_lines='skip')
 
 df = ten_years_old_with_thirty_years_experience(df)
-df = df.dropna()
+df = df.dropna()  # Not replace the nan values with medians/modes for best prediction result
 df = age_preprocess(df, 'person_age')
 df = df_label_encoder(df)
 df = col_normalization(df)
-df_balanced, X_train, X_test, y_train, y_test = sample_dataset(df, gt_col='loan_status', test_ratio=0.1)
+df_balanced, X_train, X_test, y_train, y_test = sample_dataset(df, gt_col='loan_status', test_ratio=0.1)  # will print the balanced ground truth dataframe
 
 model = CatBoostClassifier(
     iterations=4400,
-    task_type="GPU",
+    task_type="GPU",  # comment this line if using CPU
     learning_rate=0.01,
     random_seed=0
 )
@@ -28,4 +28,4 @@ model.fit(X_train, y_train,
         verbose=True
 )
 
-print(model.score(X_test, y_test))
+print(model.score(X_test, y_test))  # accuracy
